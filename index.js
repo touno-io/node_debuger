68ad1f7cbdc8019721cd8477138023392d89a09a
Kananek T <info.dvgamer@gmail.com>
2019-01-25 23:49:53 +0700

    add slack api

--- index.js
+++ index.js
@@ -20,6 +20,22 @@ if (!module.parent) {
 }
 
 const isMongo = !!process.env.MONGODB_SERVER
+const sendNotify = async (message, schedule, endpoint, apiName) => {
+  if (!message) return
+
+  let getDate = moment(!schedule ? new Date() : schedule)
+  let measure = new Time()
+  let logger = loggerCreate(apiName)
+
+  if (!isMongo) return logger[vars.DevMode ? 'log' : 'info'](getDate.format('DD-MM-YYYY HH:mm:ss'), message.replace(/\n|\n\r|\r\n/ig, '\\n'))
+
+  let { LogNotify, LogAudit } = await touno.open()
+  await new LogNotify({ endpoint: endpoint, message: message, notify: false, schedule: schedule, created: new Date() }).save()
+
+  let msg = `${apiName} ${message.length} characters save and schedule at ${getDate.format('D MMM YYYY HH:mm:ss')}. (${measure.nanoseconds()})`
+  await new LogAudit({ created: new Date(), scope: apiName, message: msg, type: 'audit', tag: [ apiName, endpoint ] }).save()
+  await logger.log(msg)
+}
 
 module.exports = {
   ...vars,
@@ -40,35 +56,14 @@ module.exports = {
       await new LogAudit({ created: new Date(), scope: 'audit', message: message, type: type, tag: tag }).save()
       await logger.log(`log`, message.length, `characters saved. (${measure.nanoseconds()})`)
     },
-    async LINE (message, schedule = new Date(), endpoint = 'Touno') {
-      if (!message) return
-
-      let measure = new Time()
-      let logger = loggerCreate('notify')
-
-      if (!isMongo) return logger[vars.DevMode ? 'log' : 'info'](moment(schedule).format('DD-MM-YYYY HH:mm:ss'), message.replace(/\n|\n\r|\r\n/ig, '\\n'))
-
-      let { LogNotify, LogAudit } = await touno.open()
-      await new LogNotify({ endpoint: endpoint, message: message, notify: false, schedule: schedule, created: new Date() }).save()
-
-      let msg = `message ${message.length} characters save and schedule at ${moment(schedule).format('D MMM YYYY HH:mm:ss')}. (${measure.nanoseconds()})`
-      await new LogAudit({ created: new Date(), scope: 'notify', message: msg, type: 'audit', tag: [ 'notify', endpoint ] }).save()
-      await logger.log(msg)
+    async LINE (message, schedule = null, endpoint = 'Touno') {
+      return sendNotify(message, schedule, endpoint, 'LINE')
+    },
+    async Slack (obj, schedule = null) {
+      return sendNotify(obj, schedule, 'Slack', 'Slack')
     },
     async Twit (message, schedule = new Date()) {
-      if (!message) return
-
-      let measure = new Time()
-      let logger = loggerCreate('twitter')
-
-      if (!isMongo) return logger[vars.DevMode ? 'log' : 'info'](moment(schedule).format('DD-MM-YYYY HH:mm:ss'), message.replace(/\n|\n\r|\r\n/ig, '\\n'))
-
-      let { LogNotify, LogAudit } = await touno.open()
-      await new LogNotify({ endpoint: 'twitter', message: message, notify: false, schedule: schedule, created: new Date() }).save()
-
-      let msg = `twitter ${message.length} characters save and schedule at ${moment(schedule).format('D MMM YYYY HH:mm:ss')}. (${measure.nanoseconds()})`
-      await new LogAudit({ created: new Date(), scope: 'twitter', message: msg, type: 'audit', tag: [ 'twitter' ] }).save()
-      await logger.log(msg)
+      return sendNotify(message, schedule, 'twitter', 'Twit')
     }
   }
 }
