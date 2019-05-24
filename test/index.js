cb58ca1aad3d5d9d5e88ef67c6c4d2a70df91b8c
Kananek Thongkam <info.dvgamer@gmail.com>
2019-05-24 15:33:29 +0700

    unit-test fixed.

--- test/index.js
+++ test/index.js
@@ -1,12 +1,13 @@
 const debuger = require('../index')
 
 const test = async () => {
-  logger = await debuger('testing')
+  logger = debuger('testing')
   logger.start('start message.')
   logger.log('log message.')
   logger.info('info message.')
   logger.error(new Error('error message.'))
   logger.success('success message.')
+  process.exit()
 }
 
 test()
