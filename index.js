eb8d319ee935c0a0d7f1e229465ea54ec193c0c0
Kananek Thongkam <info.dvgamer@gmail.com>
2019-12-25 14:00:10 +0700

    mintify code.

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 const chalk = require('chalk')
 const moment = require('moment')
-const { logging } = require('@touno-io/db/schema')
 
 const DevMode = !(process.env.NODE_ENV === 'production')
-const logSaved = process.env.MONGODB_DEBUGER === 'true'
-const logServer = process.env.MONGODB_SERVER || process.env.MONGODB_URI
+const groupSize = 6
 
-if (logSaved && !logServer) {
-  // eslint-disable-next-line no-console
-  console.log(`Please set env name 'MONGODB_URI' or 'MONGODB_SERVER'.`)
-  process.exit(1)
-}
-
-let groupSize = 5
-let scopeSize = 5
+let scopeSize = 6
 const groupPadding = (msg, size, pad) => {
   return msg.length > size ? msg.substr(0, size) : msg[pad](size, ' ')
 }
@@ -21,7 +12,6 @@ const groupPadding = (msg, size, pad) => {
 module.exports = (scopeName = null) => {
   const logWindows = (scope, icon, status, color, msg) => {
     let msg2 = [ chalk.gray(moment().format('HH:mm:ss.SSS')), color(icon) ]
-    if (status && status.length > groupSize) groupSize = status.length
     msg2.push(color(groupPadding(status, groupSize, 'padStart')))
     if (scope) {
       if (scope.length > scopeSize) scopeSize = scope.length
@@ -29,12 +19,6 @@ module.exports = (scopeName = null) => {
       msg2.push(chalk.cyan('»'))
     }
     process.stdout.write(msg2.concat(msg).join(' ') + '\n')
-    if (!logSaved) return Promise.resolve()
-    let data = { service: 'logging', status, scope, message: msg.join(' '), group: 'windows', raw: msg2.concat(msg).join(' ') }
-    const { Audit } = logging.get()
-
-    if (!Audit) return Promise.reject('Please await or call logger after scope.')
-    return new Audit(data).save()
   }
 
   const logLinux = (scope, icon, status, msg) => {
@@ -42,16 +26,10 @@ module.exports = (scopeName = null) => {
     if (scope) msg2.push(`[${scope.toUpperCase()}]`)
 
     process.stdout.write(msg2.concat(msg).join(' ') + '\n')
-    if (!logSaved) return Promise.resolve()
-
-    let data = { service: 'logging', status, scope, message: msg.join(' '), group: 'linux', raw: msg2.concat(msg).join(' ') }
-    const { Audit } = logging.get()
-    
-    if (!Audit) return Promise.reject('Please await or call logger after scope.')
-    return new Audit(data).save()
+    return Promise.resolve()
   }
 
-  const obj = {
+  return {
     log (...msg) {
       return DevMode ? logWindows(scopeName, '…', 'debug', chalk.cyan.bold, msg) : logLinux(scopeName, '…', 'debug', msg)
     },
@@ -59,10 +37,10 @@ module.exports = (scopeName = null) => {
       return DevMode ? logWindows(scopeName, '○', 'start', chalk.cyan.bold, msg) : logLinux(scopeName, '○', 'start', msg)
     },
     success (...msg) {
-      return DevMode ? logWindows(scopeName, '●', 'success', chalk.green.bold, msg) : logLinux(scopeName, '●', 'success', msg)
+      return DevMode ? logWindows(scopeName, '●', 'finish', chalk.green.bold, msg) : logLinux(scopeName, '●', 'finish', msg)
     },
     warning (...msg) {
-      return DevMode ? logWindows(scopeName, '▲', 'warning', chalk.yellow.bold, msg) : logLinux(scopeName, '▲', 'warning', msg)
+      return DevMode ? logWindows(scopeName, '▲', 'warn', chalk.yellow.bold, msg) : logLinux(scopeName, '▲', 'warn', msg)
     },
     info (...msg) {
       return DevMode ? logWindows(scopeName, '╍', 'info', chalk.blue.bold, msg) : logLinux(scopeName, null, 'info', msg)
@@ -76,5 +54,4 @@ module.exports = (scopeName = null) => {
       return DevMode ? logWindows(scopeName, 'х', 'error', chalk.red.bold, win) : logLinux(scopeName, 'х', 'error', msg)
     }
   }
-  return logSaved ? logging.open().then(() => obj) : obj
 }
