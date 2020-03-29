6dfef5365edfa7241cd012894021519f3cd064e9
Kananek Thongkam <info.dvgamer@gmail.com>
2020-03-29 16:39:44 +0700

    update logger and error.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const chalk = require('chalk')
 const f = require('date-format')
+const cli = require('cli-progress')
 
 const isWin = process.platform === 'win32'
 const isProduction = !(process.env.NODE_ENV === 'development')
 const groupSize = 6
 
 const formatDate = isProduction ? 'yyyy-MM-dd hh:mm:ss.SSS' : 'hh:mm:ss.SSS'
-let scopeSize = 6
+let scopeSize = 5
 const groupPadding = (msg, size, pad) => {
   return msg.length > size ? msg.substr(0, size) : msg[pad](size, ' ')
 }
@@ -31,6 +32,7 @@ module.exports = (scopeName = null) => {
     return Promise.resolve()
   }
 
+  let bSingleBar = null
   return {
     log (...msg) {
       return isWin ? logWindows(scopeName, '…', 'debug', chalk.cyan.bold, msg) : logLinux(scopeName, '…', 'debug', msg)
@@ -41,19 +43,61 @@ module.exports = (scopeName = null) => {
     success (...msg) {
       return isWin ? logWindows(scopeName, '●', 'finish', chalk.green.bold, msg) : logLinux(scopeName, '●', 'finish', msg)
     },
-    warning (...msg) {
+    warn (...msg) {
       return isWin ? logWindows(scopeName, '▲', 'warn', chalk.yellow.bold, msg) : logLinux(scopeName, '▲', 'warn', msg)
+    },
+    wait (msg = 'downloading', max = 100, barSize = 25) {
+      let msg2 = [ chalk.gray(f.asString(formatDate, new Date())), chalk.magentaBright('▫'), chalk.magentaBright(groupPadding('wait', groupSize, 'padStart')) ]
+
+      if (scopeName) {
+        if (scopeName.length > scopeSize) scopeSize = scopeName.length
+        msg2.push(groupPadding(scopeName, scopeSize, 'padEnd'))
+        msg2.push(chalk.cyan('»'))
+      }
+
+      if (isWin) {
+        bSingleBar = new cli.SingleBar({
+          format: msg2.join(' ') + ` ${msg} ({value}/{total}) ${chalk.gray('{bar}')} {percentage}%`,
+          barsize: barSize,
+          hideCursor: true
+        }, cli.Presets.rect)
+  
+        bSingleBar.start(max, 0)
+      } else {
+        msg2.push(msg)
+        msg2.push(`(${max}/${max})`)
+        logLinux(scopeName, '▫', 'wait', msg2)
+      }
+      return bSingleBar
+    },
+    increment (value = 1) {
+      if (bSingleBar) bSingleBar.increment(value)
+    },
+    update (value = 0) {
+      if (bSingleBar) bSingleBar.update(value)
+    },
+    stop () {
+      if (bSingleBar) bSingleBar.stop()
+      bSingleBar = null
     },
     info (...msg) {
       return isWin ? logWindows(scopeName, '╍', 'info', chalk.blue.bold, msg) : logLinux(scopeName, null, 'info', msg)
     },
-    async error (ex) {
+    error (ex) {
       if (!ex) return
-      let [ , excep ] = /at.*?\((.*?)\)/i.exec(ex.stack) || []
-      let win = [ excep ? `${excep} > ${ex.message}` : ex.message ]
-      let msg = [ (excep ? `${ex.message} :: ${excep}` : ex.message || ex) ]
-
-      return isWin ? logWindows(scopeName, 'х', 'error', chalk.red.bold, win) : logLinux(scopeName, 'х', 'error', msg)
+      if (ex.response) {
+        const { status, statusText } = ex.response
+        let msg = `${ex.config.method.toUpperCase()} ${ex.config.url} > ${status} ${statusText}`
+        return isWin ? logWindows(scopeName, 'х', 'res', chalk.red.bold, msg) : logLinux(scopeName, 'х', 'res', msg)
+      } else if (ex.request) {
+        let msg = `${ex.config.method.toUpperCase()} ${ex.config.url} > server could not be found.`
+        return isWin ? logWindows(scopeName, 'х', 'req', chalk.red.bold, msg) : logLinux(scopeName, 'х', 'req', msg)
+      } else {
+        let [ , excep ] = /at.*?\((.*?)\)/i.exec(ex.stack) || []
+        let win = [ excep ? `${excep.replace(__dirname + '\\', '')} > ${ex.message}` : ex.message ]
+        let msg = [ (excep ? `${ex.message} :: ${excep.replace(__dirname + '\\', '')}` : ex.message || ex) ]
+        return isWin ? logWindows(scopeName, 'х', 'error', chalk.red.bold, win) : logLinux(scopeName, 'х', 'error', msg)
+      }
     }
   }
 }
--- test/index.js
+++ test/index.js
@@ -1,13 +1,36 @@
 const debuger = require('../index')
+const axios = require('axios')
 
 const test = async () => {
-  logger = debuger('testing')
+  logger = debuger('test')
   logger.start('start message.')
   logger.log('log message.')
   logger.info('info message.')
   logger.error(new Error('error message.'))
-  logger.success('success message.')
-  process.exit()
+
+  try {
+    await axios.get('http://exception-request.com/')
+  } catch (ex) {
+    logger.error(ex)
+  }
+  try {
+    await axios.post('https://google.co.th/')
+  } catch (ex) {
+    logger.error(ex)
+  }
+  logger.wait()
+
+  let i = 0
+  let id = setInterval(() => {
+    logger.increment(10)
+    i++
+    if (i === 10) {
+      logger.stop()
+      logger.success('success message.')
+      clearInterval(id)
+      process.exit()
+    }
+  }, 100)
 }
 
 test()
